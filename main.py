f4c262ff3609876575bceb22ec3d5c311959c7e5
User <dossovmarlen85@gmail.com>
2024-02-18 23:44:25 +0300

    deleted logger

--- main.py
+++ main.py
@@ -2,19 +2,8 @@ import requests
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from whatsapp_api_client_python import API
-import logging
-
-
-
-# Настройка логирования
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)  # Получаем логгер для текущего файла
-
 
 app = Flask(__name__, static_folder='static')
-
-
-
 CORS(app)
 
 @app.route('/')
@@ -27,14 +16,11 @@ def get_settings():
     apiTokenInstance = request.args.get('apiTokenInstance')
     apiUrl = f"https://api.green-api.com/waInstance{idInstance}/getSettings/{apiTokenInstance}"
 
-    response = requests.get(apiUrl)  # Заголовки не требуются, так как токен передается в URL
-
-    # Возврат ответа в зависимости от статуса запроса
+    response = requests.get(apiUrl)
     if response.status_code == 200:
         return jsonify(response.json()), 200
     else:
         return jsonify({'error': 'Failed to fetch settings', 'status_code': response.status_code, 'body': response.text}), response.status_code
-
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
@@ -44,23 +30,15 @@ def send_message():
     phone_number = data.get('phone')
     message_text = data.get('message')
 
-    # Проверка наличия всех необходимых данных
     if not idInstance or not apiToken or not phone_number or not message_text:
         return jsonify({'error': 'Missing data'}), 400
 
     greenAPI = API.GreenAPI(idInstance, apiToken)
-
-    try:
-        response = greenAPI.sending.sendMessage(phone_number, message_text)
-        if response.code == 200:
-            return jsonify(response.data), 200
-        else:
-            return jsonify({'error': 'Ответ от API не успешен', 'code': response.code}), response.code
-    except Exception as e:
-        logger.error(f'Ошибка: {e}')
-        return jsonify({'error': str(e)}), 500
-
-
+    response = greenAPI.sending.sendMessage(phone_number, message_text)
+    if response.code == 200:
+        return jsonify(response.data), 200
+    else:
+        return jsonify({'error': 'Response from API was not successful', 'code': response.code}), response.code
 
 @app.route('/state_instance')
 def state_instance():
@@ -69,20 +47,14 @@ def state_instance():
     apiUrl = f"https://api.green-api.com/waInstance{idInstance}/getStateInstance/{apiTokenInstance}"
 
     response = requests.get(apiUrl)
-
-    # Логируем результат запроса
     if response.status_code == 200:
-        logger.info(f"Successfully fetched instance state: {response.json()}")
         return jsonify(response.json()), 200
     else:
-        logger.error(f"Failed to fetch instance state, status code: {response.status_code}, response: {response.text}")
         return jsonify({'error': 'Failed to fetch instance state', 'status_code': response.status_code, 'body': response.text}), response.status_code
-
 
 @app.route('/js/<path:filename>')
 def serve_js(filename):
     return send_from_directory('js', filename)
-
 
 @app.route('/send_file_by_url', methods=['POST'])
 def send_file_by_url():
@@ -102,17 +74,11 @@ def send_file_by_url():
         'body': file_url
     }
 
-    response = requests.post(f'https://api.green-api.com/waInstance{id_instance}/sendFileByUrl', headers=headers,
-                             json=payload)
-
-    # Обрабатываем ответ
+    response = requests.post(f'https://api.green-api.com/waInstance{id_instance}/sendFileByUrl', headers=headers, json=payload)
     if response.status_code == 200:
         return jsonify(response.json()), 200
     else:
-        return jsonify({'error': 'Ошибка при отправке файла', 'status_code': response.status_code}), response.status_code
+        return jsonify({'error': 'Error sending file', 'status_code': response.status_code}), response.status_code
 
-
-
-# Запустите сервер
 if __name__ == '__main__':
     app.run(debug=True)
